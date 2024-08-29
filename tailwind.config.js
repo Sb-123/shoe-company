a6e6045aacdf7fe04c84269c6b1fde18f82a40e1
Sanjeev Yadav <85263808+Sb-123@users.noreply.github.com>
2024-08-29 12:19:03 +0000

    have done some part of second section which is how-it-works

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   mode: "jit",
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        media1200: "1200px",
+        media440: "44px",
+        media560: "560px",
+        // lg: "992px",
+        // => @media (min-width:992px){ ... }
+      },
+    },
   },
   variants: {},
   plugins: [],
